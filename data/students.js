474d5154464d31bd2352b32e7837eac5d3554f24
ben-alefosio <ben_alefosio@hotmail.com>
2022-01-27 23:42:01 +1300

    add images

---
+++ data/students.js
@@ -0,0 +1,14 @@
+export default {
+  BenAlefosio: {
+    image: 'ben-alefosio.jpg',
+  },
+  JoanneLai: {
+    image: 'joanne-lai.jpg',
+  },
+  KateHann: {
+    image: 'kate-hann.jpg',
+  },
+  SamDrumm: {
+    image: 'sam-drumm.jpg',
+    }
+}
