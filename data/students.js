d079985e6c4f1d12e216903eb4b3c933c29d3a21
ben-alefosio <ben_alefosio@hotmail.com>
2022-01-28 10:25:44 +1300

    add data

--- data/students.js
+++ data/students.js
@@ -1,14 +1,30 @@
-export default {
-  BenAlefosio: {
+export default [
+  {
     image: 'ben-alefosio.jpg',
+    name: 'Ben Alefosio',
+    cohort: 'Harakeke 22',
+    starSign: 'Capricorn',
+    favFood: 'lasagne'
   },
-  JoanneLai: {
+  {
     image: 'joanne-lai.jpg',
+    name: 'Joanne Lai',
+    cohort: 'Harakeke 22',
+    starSign: 'Taurus',
+    favFood: 'sushi'
   },
-  KateHann: {
+  {
     image: 'kate-hann.jpg',
+    name: 'Kate Hann',
+    cohort: 'Harakeke 22',
+    starSign: 'Sagittarius',
+    favFood: 'pizza'
   },
-  SamDrumm: {
+  {
     image: 'sam-drumm.jpg',
-    }
-}
+    name: 'Sam Drumm',
+    cohort: 'Harakeke 22',
+    starSign: 'Aries',
+    favFood: 'fries'
+  }
+  ]
