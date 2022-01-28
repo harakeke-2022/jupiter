4c2e6dd8853798a65c12a6db57e0aaff00e7f823
Learner <bendaniels.take2@gmail.com>
2022-01-28 15:34:01 +1300

    trutn

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,8 +6,6 @@ import Aboutme from './Aboutme'
 import Lie from './Lie'
 import Truth from './Truth'
 
-
-
 function App () {
   return (
     <>
@@ -16,13 +14,13 @@ function App () {
       </div>
       {/* This 'main' div is only for styling (so we can use flexbox) */}
       <div className='main'>
-       
-        <Routes> 
+
+        <Routes>
           <Route path='/' element= { <Students /> } />
           <Route path='/aboutme' element= { <Students /> } />
           <Route path='/aboutme/:name' element={ <Aboutme /> } />
           <Route path='/truthorlie/:query' element ={ <Truth />} />
-        </Routes> 
+        </Routes>
       </div>
     </>
   )
